be3a07ec8eeee11d3cc1e6a1d1476385d65cc354
Jamie Croft <etiamsurgo@gmail.com>
2026-06-16 14:43:08 -0400

    Mobile App Begining

---
+++ mobile_app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rcinventory.app',
+  appName: 'rc-inventory',
+  webDir: 'web'
+};
+
+export default config;
